4580dcdac475574844ad21515f5880d86a4745f0
smedtr <dsmer23@gmail.com>
2022-03-15 16:19:25 +0100

    initial checkin

---
+++ posts/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+  return render(request, 'index.html')
+
+def signin(request):
+  return render(request, 'sign-in/index.html')
+
+def dashboard(request):
+  return render(request, 'dashboard/index.html')
