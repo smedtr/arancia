efb2d70e14f56aa79f105dc172f374ab0e3c92b6
smedtr <dsmer23@gmail.com>
2022-03-18 22:23:45 +0100

    add account app

---
+++ account/forms.py
@@ -0,0 +1,6 @@
+# account/forms.py
+
+from django import forms
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
--- mysite/settings.py
+++ mysite/settings.py
@@ -32,12 +32,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'account.apps.AccountConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles',   
     'posts'
 ]
 
@@ -130,3 +131,11 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Login settings 
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
+
+# EMAIL 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
