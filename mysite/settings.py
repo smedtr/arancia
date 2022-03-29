fa5f3ba491969ea4c8ea6d0983c483405caa987f
smedtr <dsmer23@gmail.com>
2022-03-29 10:26:18 +0200

    using externa env files for settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
 import os
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-frmx#d-p51(kaa7pp#o5%hhfkx*lmsgyu=z7*q)%5wcj3^rk)w'
+# Setting this value via .env file
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
