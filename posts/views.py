b93eba49795f556e732d39e84998c51ad173924c
smedtr <dsmer23@gmail.com>
2022-03-31 17:45:57 +0200

    templates restructured

--- posts/views.py
+++ posts/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-  return render(request, 'index.html')
+  return render(request, 'posts/index.html')
 
 def signin(request):
-  return render(request, 'sign-in/index.html')
+  return render(request, 'posts/sign-in/index.html')
 
 def dashboard(request):
-  return render(request, 'dashboard/index.html')
+  return render(request, 'posts/dashboard/index.html')
